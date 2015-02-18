25c691ab69a8b82d813c1c4ef2c383463026fd05
Alex Dieulot <adieulot@gmail.com>
2015-02-18 17:45:27 +0100

    Basic forum display

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
+$forum = isset($_GET['forum']) ? (int)$_GET['forum'] : false;
+$forum_slug = isset($_GET['forum_slug']) ? $_GET['forum_slug'] : false;
+
 ob_start();
-require 'views/index.php';
+if ($forum && $forum_slug)
+  require 'views/forum.php';
+else
+  require 'views/index.php';
 $body = ob_get_contents();
 ob_end_clean();
 require 'views/layout.php';
---
+++ public/views/forum.php
@@ -0,0 +1,38 @@
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_URL, "http://www.jeuxvideo.com/forums/0-{$forum}-0-1-0-1-0-{$forum_slug}.htm");
+$got = curl_exec($ch);
+
+$title = 'Communauté';
+
+$regex = '#<tr class=".*" data-id=".+">.+' .
+         '<img src="/img/forums/topic-(?P<label>.+)\.png".+' .
+         '<a href="/forums/(?P<mode>.+)-.+-(?P<topic>.+)-1-0-1-0-(?P<topic_slug>.+)\.htm" title="(?P<title>.+)">.+' .
+         '(?P<pseudo_span><span .+>)\s*(?P<pseudo>\S.*)\s*</span>.+' .
+         '<td class="dernier-msg-topic">.+<span .+>\s+(?P<date>.+)</span>.+' .
+         '.+</tr>#Usi';
+preg_match_all($regex, $got, $matches);
+# 
+#print_r($matches);
+?>
+<table>
+<?php for ($i = 0; $i < count($matches[0]); $i++): ?>
+  <tr>
+    <td><?= $matches['label'][$i] ?>
+    <td><a href="?forum=<?= $forum ?>&amp;topic=<?= $matches['topic'][$i] ?><?= $matches['mode'][$i] == 1 ? '&amp;old' : '' ?>&amp;topic_slug=<?= $matches['topic_slug'][$i] ?>"><?= $matches['title'][$i] ?></a>
+<?php
+$pseudo_status = '';
+$pos = strpos($matches['pseudo_span'][$i], ' text-');
+if ($pos) {
+  $pseudo_status = trim(substr($matches['pseudo_span'][$i], $pos + 6, 5), '"');
+  if ($pseudo_status == 'user') {
+    $pseudo_status = '';
+  }
+}
+?>
+    <td><?= $matches['pseudo'][$i] ?> <?= $pseudo_status ? "<sup>{$pseudo_status}</sup>" : '' ?>
+    <td><?= $matches['date'][$i] ?>
+<?php endfor ?>
+</table>
