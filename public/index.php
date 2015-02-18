ace3c121389cb1a34ee93c3118d613296bf7ceb1
Alex Dieulot <adieulot@gmail.com>
2015-02-18 15:40:49 +0100

    Refactor: put output buffering functions in index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
+ob_start();
 require 'views/index.php';
+$body = ob_get_contents();
+ob_end_clean();
 require 'views/layout.php';
--- public/views/index.php
+++ public/views/index.php
@@ -1,13 +1,7 @@
 <?php
 $title = 'Respeed';
-
-ob_start();
 ?>
 <h3>Forums préférés</h3>
 <ul>
   <li><a href="?forum=1000021&amp;forum_slug=communaute">Communauté</a>
 </ul>
-
-<?php
-$body = ob_get_contents();
-ob_end_clean();
