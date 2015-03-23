3406c1e73e824a8efaac3e676aeeb6b45da05244
Alex Dieulot <adieulot@gmail.com>
2015-03-23 21:38:40 +0100

    Fix blacklist_add.php

--- public/ajax/blacklist_add.php
+++ public/ajax/blacklist_add.php
@@ -1,21 +1,23 @@
 <?php
-$message_id = isset($_GET['message_id']) ? (int)$_GET['message_id'] : 0;
-if (!$message_id) {
+$id_message = isset($_GET['id_message']) ? (int)$_GET['id_message'] : 0;
+if (!$id_message) {
   exit;
 }
 
 require '../Jvc.php';
 $jvc = new Jvc();
 
-$rep = $jvc->get("http://www.jeuxvideo.com/respeed/forums/message/{$message_id}");
+$rep = $jvc->get("http://www.jeuxvideo.com/respeed/forums/message/{$id_message}");
 $location = Jvc::redirects($rep['header']);
 if($location) {
   $rep = $jvc->get("http://www.jeuxvideo.com{$location}");
 
-  if(preg_match('#<span class="picto-msg-tronche" title="Blacklister" data-id-alias="(?P<id>.+?)">#si', $rep, $matches))
+  if(preg_match('#<span class="picto-msg-tronche" title="Blacklister" data-id-alias="(?P<id>.+?)">#si', $rep, $matches)) {
     echo json_encode(
       $jvc->blacklist_add($matches['id']),
       $jvc->err()
     );
+    exit;
+  }
 }
 echo json_encode(['rep' => FALSE, 'err' => 'Message inexistant']);
