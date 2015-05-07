35a5463e30e151fd737a6c0d7a84cb93ceeed30a
Alex Dieulot <adieulot@gmail.com>
2015-05-07 15:36:17 +0200

    Update news

--- public/views/news.php
+++ public/views/news.php
@@ -19,6 +19,20 @@ $jvc = new Jvc();
   <div class="content no-menu news">
     <h1>Liste des nouveautés et changements</h1>
     
+    <h2>Mai 2015</h2>
+    
+    <h3>Jeudi 07</h3>
+    <ul>
+      <li>Refonte esthétique des champs de texte
+      <li>iOS : autocorrection enlevée quand on tape son pseudo pour se connecter
+    </ul>
+    
+    <h3>Mercredi 06</h3>
+    <ul>
+      <li>Suppression du bouton flottant pour poster (prennait trop de place sur petits écrans, et n’était pas intuitif)
+      <li>Correction d’un bug causés par les pseudos tout en chiffre
+    </ul>
+    
     <h2>Avril 2015</h2>
     
     <h3>Mercredi 22</h3>
