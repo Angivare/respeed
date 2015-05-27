f35a6ebbd8b7f6c0f274e7e3fc56974d34d0bc35
Alex Dieulot <adieulot@gmail.com>
2015-05-27 18:06:09 +0200

    Ajout du texte de la page de vente

--- public/views/salespage.php
+++ public/views/salespage.php
@@ -22,9 +22,47 @@ if ($jvc->is_connected()) {
 
 <div class="sheet">
   <div class="content no-menu">
-    <div class="presentation">
-      <h2>Bienvenue sur JVForum !</h2>
-      <p>JVForum vous permet de mieux profiter des forums de jeuxvideo.com quelque soit l’appareil que vous utilisez. <a href="/connexion">Connectez-vous</a> pour retrouver vos forums et topics préférés.</p>
+    <div class="salespage">
+
+
+      <h1>Respawn a détruit les forums.</h1>
+
+      <p>JVForum vous redonne des forums agréables à utiliser.
+
+
+      <h2>Une version mobile complète</h2>
+
+      <p>
+        <a href="http://image.noelshack.com/fichiers/2015/16/1429342667-screenshot-2015-04-18-09-04-12-jvshack.png" class="screen-mobile"><img src="http://image.noelshack.com/fichiers/2015/16/1429342667-screenshot-2015-04-18-09-04-12-jvshack.png"></img></a>
+        <a href="http://image.noelshack.com/fichiers/2015/16/1429342674-screenshot-2015-04-18-09-04-21-jvshack.png" class="screen-mobile"><img src="http://image.noelshack.com/fichiers/2015/16/1429342674-screenshot-2015-04-18-09-04-21-jvshack.png"></img></a>
+        <a href="http://image.noelshack.com/fichiers/2015/16/1429342685-screenshot-2015-04-18-09-04-54-jvshack.png" class="screen-mobile"><img src="http://image.noelshack.com/fichiers/2015/16/1429342685-screenshot-2015-04-18-09-04-54-jvshack.png"></img></a>
+
+      <p>La version mobile des forums est fortement amputée. JVForum veut vous offrir une expérience complète sur tous vos appareils.
+
+      <p>(Fait : Citer, Ignorer. À venir : Édit, MP.)
+
+
+      <h2>Une version PC adaptée aux grands écrans</h2>
+
+      <p>
+        <a href="http://image.noelshack.com/fichiers/2015/16/1429342319-capture-d-ecran-2015-04-18-a-09-26-14-jvshack.png" class="screen-desktop"><img src="http://image.noelshack.com/fichiers/2015/16/1429342319-capture-d-ecran-2015-04-18-a-09-26-14-jvshack.png"></img></a>
+        <a href="http://image.noelshack.com/fichiers/2015/16/1429342472-capture-d-ecran-2015-04-18-a-09-33-52-jvshack.png" class="screen-desktop"><img src="http://image.noelshack.com/fichiers/2015/16/1429342472-capture-d-ecran-2015-04-18-a-09-33-52-jvshack.png"></img></a>
+      
+      <p>Le design de Respawn a été pensé pour les tablettes, et gaspille l’espace disponible sur un grand écran. JVForum vous permet de voir plus d’informations d’un coup, et de moins avoir à scroller.
+
+
+      <h2>Rafraîchissement en temps réel</h2>
+
+      <p>Vous rafraîchissez souvent. JVForum le fait maintenant pour vous : les nouveaux messages sont affichés automatiquement, en temps réel.
+
+      <p>(À venir : Indicateur de nouveaux messages dans l’onglet.)
+
+
+      <h2>Un design efficient, et très rapide</h2>
+
+      <p>Respawn est une purée. JVForum se concentre sur l’essentiel et vous émancipe des folles idées de JVC et Webedia. De plus, les pages s’affichent à une vitesse folle.
+      
+      <h3><a href="/connexion">Commencez à utiliser JVForum</a></h3>
     </div>
   </div>
 </div>
