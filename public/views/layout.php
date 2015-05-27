8b061d0bb3de1487424649b6374def8eec9e61f4
Alex Dieulot <adieulot@gmail.com>
2015-05-27 20:39:53 +0200

    Update style filename

--- public/views/layout.php
+++ public/views/layout.php
@@ -2,7 +2,7 @@
 <meta charset="utf-8">
 <title><?= $title ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/style-3.css">
+<link rel="stylesheet" href="/style-4.css">
 <meta name="google-analytics-id" content="<?= GOOGLE_ANALYTICS_ID ?>">
 <?= $body ?>
 <script>
