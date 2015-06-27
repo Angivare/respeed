3cb9b5d7754b92ddf41e997f4ce44226fe1b2736
Alex Dieulot <adieulot@gmail.com>
2015-06-27 13:54:47 +0200

    Captcha input type number

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -252,7 +252,7 @@ function request_form_data() {
     form_data = data.rep
     if (form_data.fs_signature) {
       $('#captcha-container')
-      .html('<input class="input input-captcha" id="ccode" name="ccode" placeholder="Code" autocomplete="off"> <img src="/ajax/captcha_get.php?'
+      .html('<input class="input input-captcha" type="number" id="ccode" name="ccode" placeholder="Code" autocomplete="off"> <img src="/ajax/captcha_get.php?'
         + 'signature=' + encodeURIComponent(form_data.fs_signature)
         + '&hash=' + $hash + '&ts=' + $ts + '&rand=' + $rand
         + '" class="captcha">')
@@ -271,7 +271,7 @@ function request_edit_form_data(e) {
   ajax('message_edit', {id_message: id}, function(data) {
     edit_form_data = data.rep
     if (edit_form_data.fs_signature) {
-      $('#captcha-container-edit').html('<input class="input input-captcha" id="ccode_edit" name="ccode" placeholder="Code" autocomplete="off"> <img src="/ajax/captcha_get.php?'
+      $('#captcha-container-edit').html('<input class="input input-captcha" type="number" id="ccode_edit" name="ccode" placeholder="Code" autocomplete="off"> <img src="/ajax/captcha_get.php?'
         + 'signature=' + encodeURIComponent(edit_form_data.fs_signature)
         + '&hash=' + $hash + '&ts=' + $ts + '&rand=' + $rand
         + '" class="captcha">')
