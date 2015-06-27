903849723ebec2205bf236a86251e46150abe5a5
Alex Dieulot <adieulot@gmail.com>
2015-06-27 14:00:22 +0200

    Captcha input type tel pour iPhone

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,6 +9,7 @@ var form_data
   , topicRefreshes = []
   , liste_messages = liste_messages || []
   , googleAnalyticsID = $('meta[name="google-analytics-id"]').attr('content')
+  , isIPhone = navigator.userAgent.indexOf(' (iPhone; ') > -1
 
 
 
@@ -252,7 +253,7 @@ function request_form_data() {
     form_data = data.rep
     if (form_data.fs_signature) {
       $('#captcha-container')
-      .html('<input class="input input-captcha" type="number" id="ccode" name="ccode" placeholder="Code" autocomplete="off"> <img src="/ajax/captcha_get.php?'
+      .html('<input class="input input-captcha" type="' + (isIPhone ? 'tel' : 'number') + '" id="ccode" name="ccode" placeholder="Code" autocomplete="off"> <img src="/ajax/captcha_get.php?'
         + 'signature=' + encodeURIComponent(form_data.fs_signature)
         + '&hash=' + $hash + '&ts=' + $ts + '&rand=' + $rand
         + '" class="captcha">')
@@ -271,7 +272,7 @@ function request_edit_form_data(e) {
   ajax('message_edit', {id_message: id}, function(data) {
     edit_form_data = data.rep
     if (edit_form_data.fs_signature) {
-      $('#captcha-container-edit').html('<input class="input input-captcha" type="number" id="ccode_edit" name="ccode" placeholder="Code" autocomplete="off"> <img src="/ajax/captcha_get.php?'
+      $('#captcha-container-edit').html('<input class="input input-captcha" "' + (isIPhone ? 'tel' : 'number') + '" id="ccode_edit" name="ccode" placeholder="Code" autocomplete="off"> <img src="/ajax/captcha_get.php?'
         + 'signature=' + encodeURIComponent(edit_form_data.fs_signature)
         + '&hash=' + $hash + '&ts=' + $ts + '&rand=' + $rand
         + '" class="captcha">')
