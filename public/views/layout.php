0d84f8e8e8cd2efaa8ec7d8d65d67cb66461a085
Alex Dieulot <adieulot@gmail.com>
2015-06-30 16:13:27 +0200

    jQuery 2.1.4

--- public/views/layout.php
+++ public/views/layout.php
@@ -19,7 +19,7 @@ var $is_connected = <?= $jvc->is_connected() ? 'true' : 'false' ?>
   , $<?= $k ?> = '<?= $v ?>'
 <?php endforeach ?>
 </script>
-<script src="/scripts/jquery.min-<?= REVISION_NUMBER_JS_JQUERY ?>.js" data-no-instant></script>
+<script src="/scripts/jquery-<?= REVISION_NUMBER_JS_JQUERY ?>.js" data-no-instant></script>
 <script src="/scripts/fastclick-<?= REVISION_NUMBER_JS_FASTCLICK ?>.js" data-no-instant></script>
 <script src="/scripts/instantclick-<?= REVISION_NUMBER_JS_INSTANTCLICK ?>.js" data-no-instant></script>
 <script src="/scripts/jvcode-<?= REVISION_NUMBER_JS_JVCODE ?>.js" data-no-instant></script>
