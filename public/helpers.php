ec2408d4afe66cab5e4694891559920c48fb7e5a
Alexandre Dieulot <alex@macbook-pro-de-alexandre.home>
2015-07-23 22:59:44 +0200

    Date complète au hover de la date d'édition

--- public/helpers.php
+++ public/helpers.php
@@ -62,7 +62,7 @@ function adapt_html($message, $date) {
   preg_match('#</div><div class="info-edition-msg">\s*Message édité le (?P<date>.+) par\s*<span class="JvCare [0-9A-F]*" target="_blank">(?P<pseudo>.*)</span>#Usi', $message, $matches_edit);
   if ($matches_edit) {
     $message = str_replace($matches_edit[0], '', $message);
-    $message .= '<p class="edit-mention">Modifié après ' . edit_date_difference($date, $matches_edit['date']) . '</p>';
+    $message .= '<p class="edit-mention" title="' . $date . '">Modifié après ' . edit_date_difference($date, $matches_edit['date']) . '</p>';
   }
 
   // JVCare
