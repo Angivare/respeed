49d049c769de296cf9ea9ca04d2220d90048dba7
Alex Dieulot <adieulot@gmail.com>
2015-08-02 21:17:20 +0200

    Fix Google Analytics variable

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -708,7 +708,7 @@ if (googleAnalyticsID) {
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-63457513-1', 'auto');
+  ga('create', googleAnalyticsID, 'auto');
   
   InstantClick.on('change', function() {
     ga('set', 'dimension1', $is_connected ? 'Member' : 'Guest')
