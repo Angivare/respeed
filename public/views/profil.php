216817b2a9f60a30cc28e50729299c5e602f840f
Alex Dieulot <adieulot@gmail.com>
2015-10-08 16:21:48 +0200

    Handle profiles’ background images

--- public/views/profil.php
+++ public/views/profil.php
@@ -12,9 +12,15 @@ foreach (fetch_profile($pseudo) as $k => $v) {
 }
 
 $title = $pseudo;
+
+$profile_card_modifier = isset($couverture) ? 'profile-card--transparent' : '';
 ?>
-<body class="body--no-bottom"></body>
-<div class="sheet sheet--nothing-over">
+<?php if (isset($couverture)): ?>
+<body class="body--no-bottom body--sheet" style="background-image: url(<?= $couverture ?>); background-position: top center;">
+<?php else: ?>
+<body class="body--no-bottom body--sheet">
+<?php endif ?>
+
 <?php if (isset($avatar)): ?>
   <img class="js-profile-avatar profile-avatar" data-src="<?= $avatar ?>">
 <?php endif ?>
@@ -24,7 +30,7 @@ $title = $pseudo;
 <?php endif ?>
 
 <?php if (isset($messages) || isset($days)): ?>
-  <div class="profile-card">
+  <div class="profile-card <?= $profile_card_modifier ?>">
 <?php if (isset($messages)): ?>
     <div class="profile-card__messages"><strong class="profile-card__number"><?= n($messages) ?></strong> messages</div>
 <?php endif ?>
@@ -38,14 +44,14 @@ $title = $pseudo;
 <?php endif ?>
 
 <?php if (isset($signature)): ?>
-  <div class="profile-card">
+  <div class="profile-card <?= $profile_card_modifier ?>">
     <div class="profile-card__header">Signature</div>
       <?= $signature ?>
   </div>
 <?php endif ?>
 
 <?php if (isset($description)): ?>
-  <div class="profile-card profile-card--text">
+  <div class="profile-card profile-card--text <?= $profile_card_modifier ?>">
     <div class="profile-card__header">Description personnelle</div>
       <?= $description ?>
   </div>
@@ -54,4 +60,3 @@ $title = $pseudo;
   <div class="back-button-container">
     <a class="button" href="javascript:history.back()">Retour</a>
   </div>
-</div>
