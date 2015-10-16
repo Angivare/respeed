59e03392c583b0ec06a45bae41cbe391880c606d
Alex Dieulot <adieulot@gmail.com>
2015-10-16 18:34:55 +0200

    Less padding in header on mobile

--- public/views/_header.php
+++ public/views/_header.php
@@ -1,5 +1,5 @@
 <header class="header">
-  <a class="header__home-link" href="/">Accueil</a>
+  <a class="header__home-link <?= isset($forum_slug) ? 'header__home-link--accompanied' : '' ?>" href="/">Accueil</a>
 <?php if (isset($forum_slug)): ?>
   <a class="header__forum-link" href="/<?= $forum ?>-<?= $forum_slug ?>"><?=$forum_name ?></a>
 <?php endif ?>
