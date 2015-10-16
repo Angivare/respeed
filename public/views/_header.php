0171234e823612751fa7efee72f4534d8eb649a2
Alex Dieulot <adieulot@gmail.com>
2015-10-16 17:53:19 +0200

    Header as table

--- public/views/_header.php
+++ public/views/_header.php
@@ -1,6 +1,6 @@
 <header class="header">
-  <a class="button button--padded header__home-link" href="/">Accueil</a>
+  <a class="header__home-link" href="/">Accueil</a>
 <?php if (isset($forum_slug)): ?>
-  <a class="button button--padded header__forum-link" href="/<?= $forum ?>-<?= $forum_slug ?>"><?=$forum_name ?></a>
+  <a class="header__forum-link" href="/<?= $forum ?>-<?= $forum_slug ?>"><?=$forum_name ?></a>
 <?php endif ?>
 </header>
