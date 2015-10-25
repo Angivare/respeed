7acbcf8c7406e6c3b85d515b605d682ac238dc94
Alex Dieulot <adieulot@gmail.com>
2015-10-25 10:54:17 +0100

    Handle flood in cache

--- schema.sql
+++ schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE `topics_cache` (
   `topic_mode` tinyint(4) unsigned NOT NULL,
   `forum_id` int(11) unsigned NOT NULL,
   `page` smallint(6) unsigned NOT NULL,
-  `vars` text NOT NULL,
+  `vars` mediumtext NOT NULL,
   `fetched_at` double NOT NULL,
   PRIMARY KEY (`topic_id`,`topic_mode`,`forum_id`,`page`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
