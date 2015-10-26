7543b2d92dbb35829565b115488bcfafd3e6972c
Alex Dieulot <adieulot@gmail.com>
2015-10-26 10:22:36 +0100

    Smaller tap targets in header

--- public/views/_header.php
+++ public/views/_header.php
@@ -1,6 +1,10 @@
 <header class="header">
-  <a class="header__home-link <?= isset($forum_slug) ? 'header__home-link--accompanied' : '' ?>" href="/">Accueil</a>
+  <div class="header__home-cell">
+    <a class="header__home-link <?= isset($forum_slug) ? 'header__home-link--accompanied' : '' ?>" href="/">Accueil</a>
+  </div>
 <?php if (isset($forum_slug)): ?>
-  <a class="header__forum-link" href="/<?= $forum ?>-<?= $forum_slug ?>"><?=$forum_name ?></a>
+  <div class="header__forum-cell">
+    <a class="header__forum-link" href="/<?= $forum ?>-<?= $forum_slug ?>"><?=$forum_name ?></a>
+  </div>
 <?php endif ?>
 </header>
