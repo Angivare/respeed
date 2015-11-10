Angivare/respeed

commit 3066378562cfd7f118a2c55bba73bdc383d469f7

Check `pseudo` cookie in jvc->is_connected
The pseudo cookie wasn't set on login previously due to a bug

public/Jvc.php
@@ -49,7 +49,7 @@ class Jvc {
    * @return boolean TRUE si le client est connecté, FALSE sinon
    */
   public function is_connected() {
-    return isset($this->cookie['coniunctio']);
+    return isset($this->cookie['coniunctio']) && isset($_COOKIE['pseudo']);
   }
 
   /**

commit e9934dc6ba809647c96fe4f2e1e91fefbf72542f

Fix login
Regression introduced in 3066378562cfd7f118a2c55bba73bdc383d469f7

public/Jvc.php
@@ -89,7 +89,7 @@ class Jvc {
                  '&' . http_build_query($form);
     $rep = $this->request('/login', $post_data);
 
-    if ($this->is_connected()) {
+    if ($this->cookie['coniunctio']) {
       _setcookie('pseudo', $pseudo);
       Auth::refresh_uid();
       header('Location: /1000021/39674315-appli-jvforum-topic-officiel');