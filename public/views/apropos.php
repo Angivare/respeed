d91983b34b900f88d56c7eb9f7d66aa841139cf5
Alex Dieulot <adieulot@gmail.com>
2015-11-26 00:50:57 +0100

    Update à propos

--- public/views/apropos.php
+++ public/views/apropos.php
@@ -7,14 +7,18 @@ $title = 'À propos de JVForum';
   <div class="content no-menu apropos">
     <h1>À propos</h1>
 
+    <p>JVForum est actuellement utilisé pour poster 150 000 messages par mois, sois plus d’un message sur trente. C’est le projet pour améliorer les forums ayant eu le plus d’impact.
+
     <p>JVForum est le successeur de Respeed, un script pour précharger les pages. Respeed ayant des bugs impossibles à corriger et ne marchant pas sur mobile, JVForum est né.</p>
-    
+
     <p>Le développement de JVForum a commencé en mars 2015, une bêta privée a eu lieu en avril, et la sortie officielle s’est faite en mai.</p>
-    
-    <p>Le développement est fait par Alexandre, avec l’aide d’Angivare. Dans le futur JVForum sera ouvert aux contributions de tous les développeurs, <a href="https://github.com/dieulot/jvforum">sur GitHub</a>.</p>
-    
-    <p>Pour précharger les pages, JVForum utilise <a href="http://instantclick.io/">InstantClick</a> (créé aussi par Alexandre).</p>
-    
+
+    <p>Tout développeur peut contribuer au projet <a href="https://github.com/dieulot/jvforum">sur GitHub</a>.</p>
+
+    <p>Angivare a notamment participé pour coder une grosse partie du back-end, le parser de messages utilisé pour citer et modifier un message instanté, ainsi que les avatars par défaut à base de triangles.</p>
+
+    <p><a href="http://instantclick.io/">InstantClick</a> est le script qui précharge les pages et est développé par Alexandre. JVForum est utilisé pour avoir un projet réel sur lequel développer ce script.</p>
+
     <p><strong><a href="/accueil">Retour à l’accueil</a></strong></p>
   </div>
 </div>
