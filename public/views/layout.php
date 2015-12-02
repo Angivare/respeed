9de52c35602883b798ada7756c3024e9e0bb6863
Alex Dieulot <adieulot@gmail.com>
2015-12-02 16:34:07 +0100

    Change Android theme-color

--- public/views/layout.php
+++ public/views/layout.php
@@ -8,7 +8,7 @@
 <link class="js-favicon" rel="icon" href="/images/favicon.png">
 <link rel="apple-touch-icon" href="/images/appicon.png">
 <meta name="format-detection" content="telephone=no">
-<meta name="theme-color" content="#039BE5">
+<meta name="theme-color" content="#1E88E5">
 <?php if (isset($blacklist)): ?>
 <style id="blacklist-style"><?= generate_blacklist_style($blacklist) ?></style>
 <?php endif ?>
