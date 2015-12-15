a18563546a6ec9ae1472375313e313ebebb01cde
Alex Dieulot <adieulot@gmail.com>
2015-12-15 19:37:50 +0100

    Change timeout page title color

--- public/views/timeout.php
+++ public/views/timeout.php
@@ -1,10 +1,10 @@
 <div class="sheet">
   <?php include '_header.php' ?>
 
-  <div class="timeout">
-    <h3 class="timeout-title"><?= $title ?></h3>
+  <div class="content content--timeout">
+    <h3 class="page-title page-title--timeout"><?= $title ?></h3>
 
-    <p class="timeout-text"><?= $message ?></p>
+    <p><?= $message ?></p>
 
     <p>
       <a id="retry_url" class="button button--raised button--cta button--large" href="<?= $_SERVER['REQUEST_URI'] ?>" data-no-instant>Réessayer</a>
