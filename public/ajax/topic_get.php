9ec96f4249bc8e1b52fcfc96e5608910fae51b27
Alex Dieulot <adieulot@gmail.com>
2016-01-14 11:24:23 +0100

    Fix date update

--- public/ajax/topic_get.php
+++ public/ajax/topic_get.php
@@ -18,6 +18,11 @@ if (!$page) {
 
 $t = fetch_topic($topic_id_array, $page, $slug, $forum);
 
+
+for ($i = 0; $i < count($t['messages']); $i++) {
+  $t['messages'][$i]['date'] = relative_date_messages($t['messages'][$i]['dateRaw']);
+}
+
 $is_mod = $t['moderators'] && in_array(strtolower($jvc->pseudo), array_map('strtolower', $t['moderators']));
 
 if ($liste_messages) { // N’est pas là en cas de timeout
