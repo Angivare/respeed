48f2e9b6d9ec4df544f164f059cffadf83e4a792
Alex Dieulot <adieulot@gmail.com>
2016-01-14 12:04:31 +0100

    Refactor

--- public/ajax/topic_get.php
+++ public/ajax/topic_get.php
@@ -5,6 +5,10 @@ require '../parser.php';
 
 arg('forum', 'topic_mode', 'topic_id_old_or_new', 'slug', 'page', 'last_page', 'liste_messages');
 
+if (!is_array($liste_messages)) {
+  exit;
+}
+
 $topic_id_url_jvf = $topic_id_old_or_new;
 $topic_id_old = $topic_id_new = null;
 if ($topic_mode == 1) {
@@ -21,17 +25,13 @@ $t = fetch_topic($topic_id_array, $page, $slug, $forum);
 
 for ($i = 0; $i < count($t['messages']); $i++) {
   $t['messages'][$i]['date'] = relative_date_messages($t['messages'][$i]['dateRaw']);
+  if (!in_array($t['messages'][$i]['id'], $liste_messages)) {
+    $t['messages'][$i]['markup'] = generate_message_markup($t['messages'][$i], $is_mod);
+  }
 }
 
 $is_mod = $t['moderators'] && in_array(strtolower($jvc->pseudo), array_map('strtolower', $t['moderators']));
 
-if ($liste_messages) { // N’est pas là en cas de timeout
-  for ($i = 0; $i < count($t['messages']); $i++) {
-    if (!in_array($t['messages'][$i]['id'], $liste_messages)) {
-      $t['messages'][$i]['markup'] = generate_message_markup($t['messages'][$i], $is_mod);
-    }
-  }
-}
 $t['page'] = (int)$page; // Pour vérifier simplement qu’on a la bonne page dans app.js
 if ($last_page != $t['last_page']) {
   $t['paginationMarkup'] = generate_topic_pagination_markup($page, $t['last_page'], $forum, $topic_id_array, $slug);
